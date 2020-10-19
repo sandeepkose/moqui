46dd9e7d097a69b6795f931a2cc627b21b9f3423
sandeepkose <36186385+sandeepkose@users.noreply.github.com>
2020-10-19 11:59:03 +0530

    Update db-setup.sh

--- db-setup.sh
+++ db-setup.sh
@@ -1,1 +1,2 @@
 
+mysql -h DBInstance  -uDBUser -pDBPassword -e "CREATE DATABASE IF NOT EXISTS 'DBname_configuration' DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci; CREATE DATABASE 'DBname__analytical' DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
